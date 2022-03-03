fb082589d48776ab6217428486137385bbd92e42
ibrahimhuseynov <tu7pdt8l8@code.edu.az>
2022-03-03 21:29:56 +0400

    new proect

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Ayin adini daxil edin :");
+            string ad=Convert.ToString(Console.ReadLine());
+            switch (ad)
+            {
+                case "yanvar":
+                    Console.WriteLine("31");
+                    break;
+                case "fevral":
+                    Console.WriteLine("28");
+                    break;
+                case "mart":
+                    Console.WriteLine("31");
+                    break;
+                case "aprel":
+                    Console.WriteLine("30");
+                    break;
+                case "may":
+                    Console.WriteLine("31");
+                    break;
+                case "iyun":
+                    Console.WriteLine("30");
+                    break;
+                case "iyul":
+                    Console.WriteLine("31");
+                    break;
+                case "avqust":
+                    Console.WriteLine("31");
+                    break;
+                case "sentiyabr":
+                    Console.WriteLine("30");
+                    break;
+                case "oktyabr":
+                    Console.WriteLine("31");
+                    break;
+                case "noyabr":
+                    Console.WriteLine("30");
+                    break;
+                case "dekabr":
+                    Console.WriteLine("31");
+                    break;
+                        default:
+                    Console.WriteLine("bele bir ay yoxdur :" );
+                    break;
+            }
+            Console.Read(); 
+        }
+    }
+}
